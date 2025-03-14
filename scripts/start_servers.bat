fa320a0adb09b07097ff116380da9789c16aa5fb
m-henrique <mths.hddr@gmail.com>
2025-03-14 14:35:15 -0300

    fix(script)

--- scripts/start_servers.bat
+++ scripts/start_servers.bat
@@ -6,30 +6,30 @@ set PROJECT_DIR=D:\Dev\Projects\distributed-system-cfwos\
 
 :: Inicie o servidor de localização no diretório correto
 echo Starting Localization Server...
-start wt cmd /k "cd /d %PROJECT_DIR% && title Localization Server && echo Starting Localization Server on port 11110... && java -cp bin server.localization.LocalizationServer"
+start wt cmd /k "cd /d %PROJECT_DIR% && title Localization Server && echo Starting Localization Server on port 11110... && java -cp bin main.server.localization.LocalizationServer"
 
 echo Waiting for Localization Server to initialize...
 timeout /t 1 /nobreak > nul
 
 :: Inicie o servidor proxy no diretório correto
 echo Starting Proxy Server...
-start wt cmd /k "cd /d %PROJECT_DIR% && title Proxy Server && echo Starting Proxy Server on port 22220... && java -cp bin server.proxy.ProxyServer"
+start wt cmd /k "cd /d %PROJECT_DIR% && title Proxy Server && echo Starting Proxy Server on port 22220... && java -cp bin main.server.proxy.ProxyServer"
 
 echo Waiting for Proxy Server to initialize...
 timeout /t 1 /nobreak > nul
 
 : Inicie o servidor de Aplicação no diretório correto
 echo Starting Application Server...
-start wt cmd /k "cd /d %PROJECT_DIR% && title Application Server && echo Starting Application Server on port 33330... && java -cp bin server.application.ApplicationServer"
+start wt cmd /k "cd /d %PROJECT_DIR% && title Application Server && echo Starting Application Server on port 33330... && java -cp bin main.server.application.ApplicationServer"
 
 echo Waiting for Localization Server to initialize...
 timeout /t 1 /nobreak > nul
 
 :: Inicie o cliente no diretório correto
-echo Starting Client...
-start wt cmd /k "cd /d %PROJECT_DIR% && title Client && echo Starting Client... && java -cp bin client.Client"
+::echo Starting Client...
+::start wt cmd /k "cd /d %PROJECT_DIR% && title Client && echo Starting Client... && java -cp bin client.Client"
 
-echo Waiting for Client to initialize...
+::echo Waiting for Client to initialize...
 
 echo All components started.
 endlocal
